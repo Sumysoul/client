504a28d990a16647b061391fbaeac2a660f08683
Nikolay Kirichenko <mkyrychenko@magecom.us>
2024-05-06 16:31:07 +0200

    add base structure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '@/app/index.css';
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+    subsets: ['latin'],
+    variable: '--font-inter',
+});
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <div className={`page-main ${inter.variable} font-sans`}>
+            <Component {...pageProps} />
+        </div>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <h1 className="p-10">
+            Home page
+        </h1>
+    );
+}
+export default Home;
