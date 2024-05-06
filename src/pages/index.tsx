b34294b5fc3b4e98e1e93e9f16f877daedd20387
Nikolay Kirichenko <mkyrychenko@magecom.us>
2024-05-06 17:36:37 +0200

    show base data on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import axios from 'axios';
+import Image from 'next/image';
 
-function Home() {
+function Home({ data }) {
+    console.log(data);
+    const { groups } = data;
+
     return (
-        <h1 className="p-10">
-            Home page
-        </h1>
+        <div>
+            <h1 className="p-10">
+                Home page
+            </h1>
+
+            {groups.map((group) => {
+                const { imageUrl, subId } = group;
+
+                if (!imageUrl) {
+                    return null;
+                }
+
+                return (
+                    <div key={subId} className="p-10">
+                        <h3 className="font-bold mb-3">{group.name.ua}</h3>
+
+                        <Image
+                            src={imageUrl}
+                            alt={group.category}
+                            width={300}
+                            height={500}
+                        />
+                    </div>
+                );
+            })}
+        </div>
+
     );
 }
+
+export const getServerSideProps = async () => {
+    const { data } = await axios.get(`${process.env.BE_URL}/food`);
+
+    return {
+        props: { data },
+    };
+};
+
 export default Home;
