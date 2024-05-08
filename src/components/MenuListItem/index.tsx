8b720f18f94c3d56e528b6ca044090e708fce691
Nikolay Kirichenko <mkyrychenko@magecom.us>
2024-05-08 10:40:07 +0200

    add image placeholder

--- src/components/MenuListItem/index.tsx
+++ src/components/MenuListItem/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import ProductsList from '@/components/ProductsList';
+import { ImagePlaceholder } from '@/lib/imagePlaceholder';
 
 function MenuListItem({ menuItem }: any) {
     const { imageUrl, items, subId } = menuItem;
@@ -9,13 +10,14 @@ function MenuListItem({ menuItem }: any) {
         <div id={subId} className="flex flex-col pt-12">
             <h3 className="font-bold text-2xl border-b border-b-white pb-2 mb-6">{menuItem.name.ua}</h3>
 
-            <div className="w-full h-[100px] rounded-xl overflow-hidden mb-6">
+            <div className="w-full h-[100px] md:h-[80px] rounded-xl overflow-hidden mb-6 relative">
                 <Image
-                    className="!w-full !static !h-auto block max-h-full max-w-full mb-3"
                     src={imageUrl}
                     alt={menuItem.name.ua}
                     fill
                     objectFit={'cover'}
+                    placeholder="blur"
+                    blurDataURL={ImagePlaceholder}
                 />
             </div>
 
---
+++ src/lib/imagePlaceholder.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line max-len
+const ImagePlaceholder: string = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNsam+qBwAFKgIMEbowQQAAAABJRU5ErkJggg==';
+
+export { ImagePlaceholder };
