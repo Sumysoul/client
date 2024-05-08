1af0f25d7ab42bc85163d4b032afac572400982f
Nikolay Kirichenko <mkyrychenko@magecom.us>
2024-05-08 12:05:30 +0200

    fix header z-index

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import CategoriesList from '@/components/CategoriesList';
 
 function Header({ menuItems }: any) {
     return (
-        <header className="flex sticky top-0 items-center justify-center w-full p-2 bg-gray-50 shadow-lg bg-opacity-75">
+        <header className="flex sticky top-0 items-center justify-center w-full p-2 bg-gray-50 shadow-lg bg-opacity-75 z-10">
             <CategoriesList menuItems={menuItems} />
         </header>
     );
